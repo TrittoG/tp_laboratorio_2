712b295c31f90479f8bbdad78e089cca9cca90a6
TrittoG <36174147+TrittoG@users.noreply.github.com>
2018-09-30 23:33:00 -0300

    TP 2 Terminado

---
+++ TP-02/Entidades/Dulce.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Dulce : Producto
+    {
+        /// <summary>
+        /// constructor publico que recibe los Fields de la base
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Dulce(EMarca marca, string patente, ConsoleColor color) : base(patente, marca,color)
+        {
+        }
+
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+          
+        }
+
+
+        /// <summary>
+        /// override de Mostrar para dulce
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("CALORIAS : "+ this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
